5b5fb4d90ff74f33de8f95bd30f01afec7193b95
Johannes Røsvik <j.rosvik@gmail.com>
2025-09-02 21:11:45 +0200

    fix: run every 15 minutes

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -2,7 +2,7 @@ use crate::{db, nrk, rss};
 use tokio_cron_scheduler::{Job, JobScheduler};
 
 // Every 15 minutes
-const CRON_STRING: &str = "0 */15 * * * *";
+const CRON_STRING: &str = "0 1/15 * * * *";
 
 pub async fn start_scheduler() -> Result<JobScheduler, Box<dyn std::error::Error>> {
     let mut scheduler = JobScheduler::new().await?;
